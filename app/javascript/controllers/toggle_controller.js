c8e1471a3d5c85c555c384d2870c65cce1be5065
odilsonjs <italisodilsonwoodly@hotmail.com>
2024-01-16 11:28:03 -0500

    Edit the generated toggle file
    In this controller, i define a toggle method that toggles the "hidden"
     class on the target element.

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -2,6 +2,13 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="toggle"
 export default class extends Controller {
+  static targets = ["content"]
+
   connect() {
+    console.log("Toggle Controller connected");
+  }
+
+  toggle() {
+    this.contentTarget.classList.toggle("hidden");
   }
 }
